a55aaf80485e366f7d3b32258e6ddf4010a8406b
heyangZhou1997 <584069058@qq.com>
2022-08-07 23:17:52 +0800

    增加Utils注释

--- Utils.java
+++ Utils.java
@@ -11,6 +11,11 @@ public class Utils {
     }
 
 
+    /***
+     * 用于打印int数组的每个值
+     *
+     * @param array int数组
+     */
     public static void print(int[] array) {
         for (int a : array) {
             System.out.println(a);
